c5fcb1d95e9647c583739fbcd14fb2a76fed9a0b
Anton Mikaskin <garrakombo@gmail.com>
2024-11-29 10:39:26 +0300

    f

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 8080;
@@ -23,7 +24,7 @@ async function init() {
     res.json(promocode);
   });
 
-  app.use(express.static('public'));
+  app.use('/', express.static(path.join(__dirname, 'public')));
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
